519fca572004c17013de9f974274180f32c9c9de
Montchat <je4440@gmail.com>
2015-10-01 15:46:22 -0400

    cleaned up files
    - cleaned up code and removed notes
    - made it easier to read
    - need to fix the button tag functionality

--- TouchDraw/DrawView.swift
+++ TouchDraw/DrawView.swift
@@ -11,70 +11,19 @@ import UIKit
 class DrawView: UIView {
     
     var lines = [Line]()
-    //Can we put scribbles in the line array? Yes, because anything that a line can do a scribble can do also because it is a subclass of a line. (and more)
     override func drawRect(rect: CGRect) {
-        
-//different fills and strokes to create images in a view.
-//the different Fills and Strokes do different things. For instance, the fill will create a shape that completely fills the shape. a stroke will only do the border around the shape. an ellipse will create a circle. for all of these we need a context
-        
-//          Part 1(needs to be uncommented)
-//        let context = UIGraphicsGetCurrentContext()
-//        UIColor.magentaColor().set()
-//        CGContextFillRect(context, CGRect(x: 10, y: 10, width: 100, height: 100))
-//        CGContextStrokeRect(context, CGRect(x: 200, y: 10, width: 100, height: 100))
-//        CGContextFillEllipseInRect(context, CGRect(x: 100, y: 150, width: 100, height: 100))
-//        
-//        CGContextAddEllipseInRect(context, CGRect(x: 20, y: 300, width: 200, height: 200))
-//        CGContextFillPath(context)
-//        
-//        CGContextAddEllipseInRect(context, CGRect(x: 20, y: 300, width: 200, height: 200))
-//        CGContextStrokePath(context)
-//        
-//        CGContextAddEllipseInRect(context, CGRect(x: 100, y: 300, width: 200, height: 200))
-//        UIColor.cyanColor().set()
-//        CGContextFillPath(context)
-//
-
-//       Part 2: Drawing Lines
-//        let context = UIGraphicsGetCurrentContext()
-//        UIColor.magentaColor().set()
-//        CGContextMoveToPoint(context, 20, 20)
-//        CGContextAddLineToPoint(context, 100, 100)
-//        
-//        //CGContextStrokePath(context) (commented out)
-//        
-//        //The code above is all one path that we are drawing. The View starts from (0,0). We set the start of the point to (20,20). It draws a line to (100,100)
-//        
-//        CGContextMoveToPoint(context, 200, 200)
-//        CGContextAddLineToPoint(context, 200, 100)
-//        
-//        CGContextStrokePath(context)
-//        
-        //The code above will create a new line that starts at point (200,200). and draws backwards to (200,100).
-        
-        
-//      Part 3: Multiple Lines
-
         let context = UIGraphicsGetCurrentContext()
         
         UIColor.magentaColor().set()
-       
-        //        Above, we are pulling in creating instances of multipleLines from the class Line below
-        
-        //We need a loop for an array so that we can draw multiple lines from our array
         
         for line in lines {
-            //looping line in lines
             if let start = line.start, let end = line.end {
                 if let fillColor = line.fillColor {
-                    //if we have a fill color, we are going to do something with the fill
                     fillColor.set()
-                    
                     if let shape = line as? Shape {
-                        
                         let width = end.x - start.x
                         let height = end.y - start.y
-                        
+                    
                         let rect = CGRect(x: start.x, y: start.y, width: width, height: height)
                         
                         switch shape.type ?? .Rectangle {
@@ -96,21 +45,11 @@ class DrawView: UIView {
                         case .Diamond:
 
                             let top = CGPoint(x: width / 2 + start.x , y: start.y)
-                            let right = end
-                            let left = CGPoint(x: start.x, y: end.y)
-                            
-//                            let midX = CGRectGetMidX(rect)
-//                            let midY = CGRectGetMidY(rect)
-//
-//
-//                            print(top)
-//                            print(right)
-//                            print(left)
+                            let right = CGPoint(x: end.x, y:height / 2 + start.y)
+                            let left = CGPoint(x:start.x, y:height / 2 + start.y)
+                            let bottom = CGPoint(x: width / 2 + start.x, y:end.y)
                             
                             CGContextMoveToPoint(context, top.x, top.y)
-                            CGContextAddLineToPoint(context, right.x, right.y)
-//                            CGContextAddLineToPoint(context, bottom.x, bottom.y)
-
                             CGContextFillPath(context)
                             
                         }
@@ -122,33 +61,21 @@ class DrawView: UIView {
                 if let strokeColor = line.strokeColor {
                     
                     CGContextSetLineWidth(context, line.strokeWidth)
-                    //setting the line width
                     
                     CGContextSetLineCap(context, .Round)
                     CGContextSetLineJoin(context, .Round)
                     
-                    //if we have a stroke color, we are going to do something with the stroke
                     strokeColor.set()
-                    //setting the color of the lines
                     CGContextMoveToPoint(context, start.x, start.y)
-                    //start
                     
-//                    if line is Scribble {
-//                        // if the line is of type Scribble do this //***new thing here. "is" is checking for Type
-//                    
-//                    }
                     if let scribble = line as? Scribble {
-                        // if it is a Scribble...
                         CGContextAddLines(context, scribble.points, scribble.points.count)
-                        //add the points to the line
                         
                     }
                     
                     CGContextAddLineToPoint(context, end.x, end.y)
-                    //end line if not a Scribble
-                    
                     CGContextStrokePath(context)
-                    //stroke the path
+                    
                     
                 }
                 
@@ -160,7 +87,7 @@ class DrawView: UIView {
     
 }
 
-//Creating a Line Class: for multiple lines
+
 class Line {
     
     var start: CGPoint?
@@ -170,20 +97,15 @@ class Line {
     var fillColor: UIColor?
     
     var strokeWidth: CGFloat = 0
-    //Every class of CoreGraphics is going to be using CGFloat, which is why we are using CGFloat over Int (//strokeWidth: Int = 0)
     
 }
-
-
-//Creating a SubClass of the Line, called the Stroke. this is made so that instead of straight lines, we can just scribble on the screens
 
 class Scribble: Line {
     var points = [CGPoint]() {
         didSet {
             start = points.first
             end = points.last
-            //a line is going to have a start and an end. but when we scribble we can technically keep adding points to the array of points, and we can set the points to the first and last item in that array.
-            
+
         }
         
     }
@@ -197,12 +119,9 @@ enum ShapeType {
 
 class Shape: Line {
     var type: ShapeType!
-    
     init(type:ShapeType) {
         self.type = type
         
     }
     
 }
-
-//Other notes. The viewController send the message to the view object telling it to draw. It needs to update its view, and we need to tell it what lines are there too
--- TouchDraw/ViewController.swift
+++ TouchDraw/ViewController.swift
@@ -12,29 +12,21 @@ class ViewController: UIViewController {
     var chosenTool: Int = 0
     
     @IBOutlet weak var controlPanelView: UIView!
-    //control pannel view
-    
     
     @IBOutlet weak var controlPanelTop: NSLayoutConstraint!
-    //the gray area on the top of our app
     
     @IBAction func toggleControlPanel(sender: AnyObject) {
-        //the apple button that will toggle our control panel to move up
         self.controlPanelTop.constant = self.controlPanelView.frame.origin.y == 0 ? -300: 0
         view.setNeedsUpdateConstraints()
-        
         UIView.animateWithDuration(1) { () -> Void in
-            //providing an animation so that when the controlPannel goes up, it does so slowly, and doesn't look as jarring
             self.view.layoutIfNeeded()
             
         }
         
     }
     
-    
     @IBAction func chooseTool(button: UIButton) {
         chosenTool = button.tag
-        
         
     }
     
@@ -55,12 +47,10 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        //UITouch represents the location, size, movement, and force on a finger on the screen for a particular event. The force of a touch is available starting in iOS 9 on their devices that suppo rts 3D Touch
-            if let touch = touches.first {
+           if let touch = touches.first {
                 switch chosenTool {
                 case 1:
                     let newScribble = Scribble()
@@ -91,99 +81,22 @@ class ViewController: UIViewController {
                     (view as? DrawView)?.lines.append(newLine)
                     
                 }
-                
-//                
-//                
-//            // this is an optional UITouch which sets a touch value equal to the first touch in the array
-////            let newLine = Line()
-//            // creating a new instance object o the line
-////            let locationPoint = touch.locationInView(view)
-////            //setting the touch location to a locationPoint. this is a CGPoint
-////            newLine.start = locationPoint
-//            //setting the start of the line to the location point (the CGPoint that the user touches)
-//            
-//            //we take the two lines commented out above and we simplify them into one line
-//            
-////            newLine.start = touch.locationInView(view)
-////            newLine.strokeColor = UIColor.blackColor()
-////            newLine.strokeWidth = 10
-////            //setting the other parameters of the view
-////            
-////            (view as? DrawView)?.lines.append(newLine)
-//////                adding a line to the line array
-//                
-//            //Adding a Scribble to the viewController
-////                let newScribble = Scribble()
-////                newScribble.points.append(touch.locationInView(view))
-////                newScribble.strokeColor = UIColor.blackColor()
-////                newScribble.strokeWidth = 10
-////                
-////                (view as? DrawView)?.lines.append(newScribble)
-////
-//////            
-////        }
-//                let shape = Shape(type: .Diamond)
-//                //Adding a triangle to the view
-//                shape.start = touch.locationInView(view)
-//                shape.fillColor = UIColor.blackColor()
-//                //adding  black circle to the view
-//                (view as? DrawView)?.lines.append(shape)
-//                
-//                view.setNeedsDisplay()
-            
-                
         }
-        
+    }
 
-
-        //displays the
-
-//        for touch in touches {
-//            //looping for the touches
-//            
-//        }
-        
-        }
-    
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if let touch = touches.first {
             
             if let currentScribble = (view as? DrawView)?.lines.last as? Scribble {
-                //if it is a scribble, do this:
                 currentScribble.points.append(touch.locationInView(view))
                 view.setNeedsDisplay()
                 
             } else if let currentLine = (view as? DrawView)?.lines.last {
-                //if it is not a scribble (is a line) then do this:
                 currentLine.end = touch.locationInView(view)
                 view.setNeedsDisplay()
                 
             }
             
-            //setting our touch parameter to the first touch in the array. conditionally unwrapping it.
-//            if let currentLine = (view as? DrawView)?.lines.last {
-//                //setting the parameter of the endpoint of the line
-//                currentLine.end = touch.locationInView(view)
-//                
-//                view.setNeedsDisplay()
-//                //Maks the receiver's entire bounds rectangle as needing to be redrawn.// this resets the draw Rect, so that we can draw the line
-//     
-//                
-//            if let currentScribble = (view as? DrawView)?.lines.last as? Scribble {
-//                currentScribble.points.append(touch.locationInView(view))
-//                //adding the location to the scribble points
-//                view.setNeedsDisplay()
-//                //refreshing the view
-//            }
-
-            //Shape:
-            
-//            if let currentShape = (view as? DrawView)?.lines.last {
-//                currentShape.end = touch.locationInView(view)
-//                view.setNeedsDisplay()
-
-                
-//            }
 
         }
         
